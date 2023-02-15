c4e8169e3087c6cdf31064dab2d17be5662a12d5
SebastianMark32 <smarkgolfer@gmail.com>
2023-02-15 14:14:23 -0500

    Cleaned up

--- findingVowel.cpp
+++ findingVowel.cpp
@@ -17,23 +17,23 @@ void vowelCheck(char vowel){
   // checking for lower case vowels
   if (vowel == 'a' || vowel == 'e' || vowel == 'i' || vowel == 'o' || vowel =='u'){
    
-    std::cout << "This character is lower case vowel" << std::endl;
+     std::cout << "This character is lower case vowel" << std::endl;
  
     // checking for upper case vowels
   } else if ( vowel == 'A' || vowel == 'E' || vowel == 'I' || vowel == 'O' || vowel =='U'){
    
-    std::cout << "This character is an UPPER case vowel" << std::endl;
+      std::cout << "This character is an UPPER case vowel" << std::endl;
  
-
   // if not an upper or lower caser vowel then its a consonant
   } else std::cout << "This character is a consonant" << std::endl;
 }
+
 int main(){
 
   std::string userInput;
 
-// prompting user input until exit flag
-while(true){
+  // prompting user input until exit flag
+  while(true){
   
   // Taking a char input from a user
   std::cout << "Enter a character: " << std::endl;
@@ -42,13 +42,16 @@ while(true){
   // I do not think -1 exit flag is possible since we are using chars
   if(userInput == "-1"){
     std::cout << "Thank you for play the vowel game!" << std::endl;
-    return 0;
 } 
   char vowel = userInput[0];
 
   if(isalpha(vowel)){
-  vowelCheck(vowel);
+ 
+    vowelCheck(vowel);
+ 
   } else std::cout << "Try to enter a letter" << std::endl;
 
   }
+
+return 0;
 }
