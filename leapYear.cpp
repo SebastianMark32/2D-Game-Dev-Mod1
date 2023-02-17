8bf587a4550cebff7e4d285f7fb329f14f3a2181
SebastianMark32 <smarkgolfer@gmail.com>
2023-02-17 14:27:13 -0500

    Changed some logic

--- leapYear.cpp
+++ leapYear.cpp
@@ -9,12 +9,14 @@
  * Professor Roy
  * ****************************************/
 
-
 #include <iostream>
 
 void leapYearChecker(int year){
 
-if(year % 4 == 0 || year % 100 == 0 && year % 400 != 0){
+// checks leap year criteria
+// if divisible by 4 and NOT 100 and divisible 400 then its a leap year
+// example 2000 is a leap year 
+if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
   std::cout << "This year is a leap year" << std::endl;
 } else std::cout << "Not a leap year" << std::endl;
 
@@ -24,6 +26,7 @@ int main(){
 
   int year;
   std::cout << "Enter a year:" << std::endl;
+  // taking user input 
   std::cin >> year;
   leapYearChecker(year);
 
