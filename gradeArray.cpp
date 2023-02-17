2ee7804843931f0b7ce3bf4eaaea54450ec3741d
SebastianMark32 <smarkgolfer@gmail.com>
2023-02-17 13:45:55 -0500

    Changed comments

--- gradeArray.cpp
+++ gradeArray.cpp
@@ -9,39 +9,38 @@
  * Professor Roy
  * ***************************************************/
 
-#include <iostream>
-#include <iomanip>
 #include <cmath>
+#include <iomanip>
+#include <iostream>
 
-int main(){
+int main() {
 
-  
-float grade[5];
-float sum = 0.0;
-float average = 0.0;
+  float grade[5];
+  float sum = 0.0;
+  float average = 0.0;
 
-// Asking the user to take an input
-std::cout << "Enter a grade " << std::endl;
-for(int i = 1; i <= 5; i++){
+  // Taking user input
+  std::cout << "Enter a grade " << std::endl;
+  for (int i = 1; i <= 5; i++) {
 
-  std::cout << "Grade:" << i << std::endl;
-  std::cin >> grade[i];
+    std::cout << "Grade:" << i << std::endl;
+    std::cin >> grade[i];
 
-  // summing the first i indecies
-  // summing the first 5 elements starting from 0 until 4
-  sum += grade[i];
-}
+    // summing the first i indicies
+    // summing the first 5 elements starting from 0 until 4
+    sum += grade[i];
+  }
 
-float size = sizeof(grade)/sizeof(grade[0]);
+  float size = sizeof(grade) / sizeof(grade[0]);
 
-// dividing by the total elements in the array i.e., 5
-average = sum/size;
+  // dividing by the total elements in the array i.e., 5
+  average = sum / size;
 
-// truncating the decimals values to four
-std::cout << std::setprecision(4) << std::endl;
+  // truncating the decimals values four places
+  std::cout << std::setprecision(4) << std::endl;
 
-// printing the final result
-std::cout << "The average is: " << average << std::endl;
+  // printing average
+  std::cout << "The average is: " << average << std::endl;
 
-return 0;
+  return 0;
 }
