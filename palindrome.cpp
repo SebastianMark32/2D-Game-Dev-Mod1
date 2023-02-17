7445694e4550e348b3a184b6089ac3b9bab0b54c
SebastianMark32 <smarkgolfer@gmail.com>
2023-02-17 16:48:48 -0500

    Added a few parts

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,32 +1,63 @@
+/*******************************************************************
+ *This program checks if a word is a palindrome.
+ *Example:  tacocat can be read from right to left or left to right.
+ *
+ * *Note: I added two break statements, otherwise 
+ * the printout will replicated based on the size of the word. 
+ * Its kind of a ducktape approach, however I think it works.
+ *
+ * **Disclaimer** I have seen this problem many times and
+ * during my ISP I did this problem in C. Therefore, I cannot take 
+ * credit for my alogrithm. 
+ *
+ * @author Sebastian Mark
+ *
+ * C++ 2D Game Development
+ *
+ * Professor Roy
+ * ****************************************************************/
+
+#include <cstring>
 #include <iostream>
+#include <string>
 
-using namespace std;
+void isPalindrome(char string[]) {
 
-void isPalindrome(){
+  // divides the character array in half
+  int middle = strlen(string)/2;
   
-  int number, remainder, quotient;
-  int result = 0;
-
-  cout << "Enter a number" << endl;
-  cin >> number;
-
-  quotient = number;
-
-  while(quotient != number){
-    remainder = quotient%10;
-    result = result * 10 + remainder;
-    quotient = quotient/10;
-  } if (result == 0){
-    cout << "Palindrome!" << endl;
-  } else cout << "Not palindrome" << endl;
   
+  int length = strlen(string);
+  
+  // iterates through each element until the mid-point
+  for (int i = 0; i < middle; i++) {
+
+  // checking if the elements are not equal 
+  // through the entire array
+    if (string[i] != string[length - i - 1]) {
+      std::cout << "Not a palindrome" << std::endl;
+      
+      // reduce duplicates
+      break;
+    } else
+      std::cout << "Palindrome!" << std::endl; break;
+  }
 }
 
-int main(){
+int main() {
 
+  char word[100];
+  while (true) {
+    std::cout << "Enter a word into the palindrome 3000:" << std::endl;
+    std::cin >> word;
 
-isPalindrome();
-  
+    // exit flag to break out of the loop
+    if (strcmp(word, "N") == 0 || strcmp(word, "n") == 0) {
+      break;
+    }
 
-return 0;
+    isPalindrome(word);
+  }
+
+  return 0;
 }
